09606be13f2302dba6b799a100fd491cdef4e340
artemyv <artemyv@gmail.com>
2025-08-01 15:18:32 +0300

    Provide operator to check that LoadLibrary failed Make constructor noexcept

--- dllhelper.hpp
+++ dllhelper.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
 #include <type_traits>
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <stdexcept>
 
 class ProcPtr {
 public:
-  explicit ProcPtr(FARPROC ptr) : _ptr(ptr) {}
+  explicit ProcPtr(FARPROC ptr) noexcept : _ptr(ptr) {}
 
   template <typename T, typename = std::enable_if_t<std::is_function_v<T>>>
-  operator T *() const {
+  operator T *() const noexcept {
+#pragma warning(push)
+#pragma warning(disable : 26490) 
     return reinterpret_cast<T *>(_ptr);
+#pragma warning(pop)
   }
 
 private:
@@ -19,20 +22,35 @@ private:
 
 class DllHelper {
 public:
-  explicit DllHelper(LPCTSTR filename) : _module(LoadLibrary(filename)) {
-    if (NULL == _module) {
-      throw std::runtime_error("Library not loaded");
-    }
+  explicit DllHelper(const wchar_t* filename) noexcept : _module(LoadLibraryW(filename)) 
+  {
   }
 
   ~DllHelper() { FreeLibrary(_module); }
-
-  ProcPtr operator[](LPCSTR proc_name) const {
+  DllHelper() = delete;
+  DllHelper(const DllHelper&) = delete; // Copy constructor
+  DllHelper& operator=(const DllHelper&) = delete; // Copy assignment
+  DllHelper(DllHelper&& other) noexcept
+  {
+      std::swap(_module,other._module);
+  }
+  ; // Move constructor
+  DllHelper& operator=(DllHelper&& other) noexcept// Move assignment
+  {
+      if(this != &other) {
+          FreeLibrary(_module);
+          _module = nullptr;
+          std::swap(_module, other._module);
+	  }
+	  return *this;
+  }
+  operator bool() const noexcept {
+    return _module != nullptr;
+  }
+  ProcPtr operator[](LPCSTR proc_name) const noexcept {
     return ProcPtr(GetProcAddress(_module, proc_name));
   }
 
-  static HMODULE _parent_module;
-
 private:
-  HMODULE _module;
+  HMODULE _module = nullptr;
 };
