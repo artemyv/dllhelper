df858e0d1af25f586c84f335b2df3b5f31f3bb50
artemyv <artemyv@gmail.com>
2025-08-08 16:06:19 +0300

    Fixes for C-code example

--- src/example_win.c
+++ src/example_win.c
@@ -1,14 +1,27 @@
 #include <windows.h>
+#include <stdio.h>
 
-typedef int(WINAPI *ShellAboutProc)(HWND, LPCSTR, LPCSTR, HICON);
+typedef int(WINAPI *ShellAboutProcW)(HWND, LPCWSTR, LPCWSTR, HICON);
 
 int main() {
   HMODULE hModule = LoadLibrary(TEXT("Shell32.dll"));
 
-  ShellAboutProc shellAbout =
-      (ShellAboutProc)GetProcAddress(hModule, "ShellAboutA");
-
-  shellAbout(NULL, "hello", "world", NULL);
+  if (hModule == NULL) {
+    // Handle error
+    DWORD error = GetLastError();
+    printf( "Failed to load Shell32.dll, error code: %d\n", error);
+    return 1;
+  }
+  ShellAboutProcW shellAbout =
+      (ShellAboutProcW)GetProcAddress(hModule, "ShellAboutW");
+  if(shellAbout == NULL) {
+    // Handle error
+    DWORD error = GetLastError();
+    printf("Failed to get address of ShellAboutW, error code: %d\n", error);
+    FreeLibrary(hModule);
+    return 1;
+  }
+  shellAbout(NULL, L"hello", L"world", NULL);
 
   FreeLibrary(hModule);
 }
